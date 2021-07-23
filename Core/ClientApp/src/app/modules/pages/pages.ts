1681ead5ad68c341215018e3520e30a76417094b
emre1702 <emre1702@live.de>
2021-07-23 19:18:46 +0200

    Hide navigation if not logged in

--- Core/ClientApp/src/app/modules/pages/pages.ts
+++ Core/ClientApp/src/app/modules/pages/pages.ts
@@ -1,5 +1,5 @@
 import routerRoutes from 'src/app/router.routes';
 
-const pages: { text: string; route: string }[] = [{ text: 'Home', route: routerRoutes.home }];
+const pages: { text: string; route: string }[] = [{ text: $localize`Home`, route: routerRoutes.home }];
 
 export default pages;
