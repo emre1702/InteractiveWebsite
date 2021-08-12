c6cfefcccf858b8a8d278e95e85c4837b94a522b
emre1702 <emre1702@live.de>
2021-08-12 22:54:54 +0200

    Fix a warning

--- Core/Program.cs
+++ Core/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace InteractiveWebsite.Core
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
